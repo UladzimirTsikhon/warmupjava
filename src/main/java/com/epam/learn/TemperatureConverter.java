ba2a5366679f810d9e58827be5ccadebdbbb73a7
Pavel Arlovich <porlovich@gmail.com>
2018-11-13 16:46:22 +0300

    fix test

--- src/main/java/com/epam/learn/TemperatureConverter.java
+++ src/main/java/com/epam/learn/TemperatureConverter.java
@@ -5,6 +5,7 @@ public class TemperatureConverter {
      * celsius to fahrenheit
      */
     public int toFahrenheit(int celsius) {
-        return celsius >= 0 ? (celsius * 9/5) + 32 : (celsius * 9/5) + 31;
+//        return celsius >= 0 ? (celsius * 9/5) + 32 : (celsius * 9/5) + 31;
+        return (celsius * 9/5) + 32;
     }
 }
