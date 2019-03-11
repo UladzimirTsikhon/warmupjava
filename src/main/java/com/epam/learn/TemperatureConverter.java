7e601a4c5e82b9b7f2762f398c45909fd054241a
Vladimir Tsikhon <uladzimir_tsikhon@epam.com>
2019-03-11 15:20:33 +0300

    Update TemperatureConverter.java

--- src/main/java/com/epam/learn/TemperatureConverter.java
+++ src/main/java/com/epam/learn/TemperatureConverter.java
@@ -7,6 +7,6 @@ public class TemperatureConverter {
     public int toFahrenheit(int celsius) {
 //        return celsius >= 0 ? (celsius * 9/5) + 32 : (celsius * 9/5) + 31;
         //return (celsius * 9/5) + 32;
-        return (celsius + 32);
+        return 3;
     }
 }
